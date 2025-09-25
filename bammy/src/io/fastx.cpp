69c3977c7e8dd394ab43b564a2e30f9e6b31c60d
Javier Arambula Rascon <ja1473@Javiers-MacBook-Pro.local>
2025-09-24 21:43:28 -0400

    project scaffold

---
+++ bammy/src/io/fastx.cpp
@@ -0,0 +1,8 @@
+#include "fastx.hpp"
+
+FastIO::FastIO() {
+}
+
+void FastIO::test() {
+    std::cout << "Hello from FastIO" << std::endl;
+}
---
+++ bammy/src/io/fastx.hpp
@@ -0,0 +1,12 @@
+#ifndef FASTX_HPP
+#define FASTX_HPP
+
+#include <iostream>
+
+struct FastIO {
+    FastIO();
+
+    static void test();
+};
+
+#endif
